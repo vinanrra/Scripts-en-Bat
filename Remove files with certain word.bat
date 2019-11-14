7b4b8051aa9cb391405ac9d75a89757fc8a37e13
vinanrra <vicentefernandezguerra@gmail.com>
2019-11-14 13:09:51 +0100

    Small fixes and info added.

--- Remove files with certain word.bat
+++ Remove files with certain word.bat
@@ -1,26 +1,32 @@
 @echo off & setlocal
 title Script made by Vinanrra - Last modification: 14/11/2019
 color 0a
+
 echo String that file must contain
 set /p MySearchString=
-echo Directory to move (Complete path)
+
+echo ============================================================================================
+echo If you want to move to a directory inside this folder just type the name of the folder
+echo otherwise put the complete path to the new folder
+echo ============================================================================================
+echo Directory to move
 set /p Directory=
+
 cls
-============================================================================
+============================================================================================
 for /f "delims=" %%i in ('dir /b /s /a-d ^| findstr /i "%MySearchString%"') do echo move "%%~i" %Directory%
-============================================================================
-echo Is this correct?
-echo 1) Yes
-echo 2) No
-echo Choose an option
-set /p op=
-if "%op%"=="1" goto Continue
-if "%op%"=="2" goto Exit
+============================================================================================
+
+set /p op=Is this correct? [Y/n]: 
+if "%op%"=="Y" goto Continue
+if "%op%"=="n" goto Exit
+
 :Continue
 mkdir %Directory%
 for /f "delims=" %%i in ('dir /b /s /a-d ^| findstr /i "%MySearchString%"') do move "%%~i" %Directory%
 echo Files moved to %Directory%
 pause
+
 :Exit
 pause
 @exit
