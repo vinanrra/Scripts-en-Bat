f0b5fd4f4fc53f0f9bde581425e4c71f31271e66
vinanrra <vicentefernandezguerra@gmail.com>
2019-11-14 13:01:09 +0100

    Added variables and confirmation

--- Remove files with certain word.bat
+++ Remove files with certain word.bat
@@ -1,6 +1,26 @@
-# First try with the "echo" to check the files that will be removed
-# Second remove "echo" to remove files
-
 @echo off & setlocal
-set "MySearchString=X"
-for /f "delims=" %%i in ('dir /b /s /a-d ^| findstr /i "%MySearchString%"') do echo del "%%~i"
+title Script made by Vinanrra - Last modification: 14/11/2019
+color 0a
+echo String that file must contain
+set /p MySearchString=
+echo Directory to move (Complete path)
+set /p Directory=
+cls
+============================================================================
+for /f "delims=" %%i in ('dir /b /s /a-d ^| findstr /i "%MySearchString%"') do echo move "%%~i" %Directory%
+============================================================================
+echo Is this correct?
+echo 1) Yes
+echo 2) No
+echo Choose an option
+set /p op=
+if "%op%"=="1" goto Continue
+if "%op%"=="2" goto Exit
+:Continue
+mkdir %Directory%
+for /f "delims=" %%i in ('dir /b /s /a-d ^| findstr /i "%MySearchString%"') do move "%%~i" %Directory%
+echo Files moved to %Directory%
+pause
+:Exit
+pause
+@exit
